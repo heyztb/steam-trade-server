a199950778d5c07377c1d256fd4f573f7f3d0699
Zach Blake <zblake@sightlinepayments.com>
2023-01-24 15:02:44 -0800

    connect to postgres and pass it to our handler

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,10 +1,16 @@
 package router
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/heyztb/steam-trade-server/internal/handlers"
 )
 
 // NewRouter creates a new router for the spec and the given handlers.
@@ -14,12 +20,30 @@ import (
 //
 // 0.1.0
 func NewRouter() http.Handler {
+	postgresDSN := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		"localhost",
+		5432,
+		"postgres",
+		"",
+		"steam_trade_server")
+
+	db, err := sql.Open("postgres", postgresDSN)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello world"))
-	})
+	r.Use(middleware.Recoverer)
+
+	r.Method("POST", "/trade/new", handlers.NewCreateTradeOfferHandler(db, log.Default()))
 
 	return r
 }
