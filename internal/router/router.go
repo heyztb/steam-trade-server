5d183159a55007d310e6874e9a2aed699656ca7a
Zach Blake <zblake@sightlinepayments.com>
2023-01-24 20:55:48 -0800

    use environment variables

--- internal/router/router.go
+++ internal/router/router.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	_ "github.com/lib/pq"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/heyztb/steam-trade-server/internal/handlers"
+	_ "github.com/joho/godotenv/autoload"
 )
 
 // NewRouter creates a new router for the spec and the given handlers.
@@ -21,12 +23,12 @@ import (
 // 0.1.0
 func NewRouter() http.Handler {
 	postgresDSN := fmt.Sprintf(
-		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		"localhost",
-		5432,
-		"postgres",
-		"",
-		"steam_trade_server")
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_NAME"))
 
 	db, err := sql.Open("postgres", postgresDSN)
 	if err != nil {
