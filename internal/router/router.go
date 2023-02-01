3a0d93330bd048158a428cee1b3301704e51f0b7
Zach Blake <zblake@sightlinepayments.com>
2023-02-01 15:14:18 -0800

    switch to sqlite3

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,17 +2,14 @@ package router
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 
-	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/heyztb/steam-trade-server/internal/handlers"
-	_ "github.com/joho/godotenv/autoload"
 )
 
 // NewRouter creates a new router for the spec and the given handlers.
@@ -21,16 +18,10 @@ import (
 // # RESTful API to enable users to trade Steam items to and from automated trading accounts
 //
 // 0.1.0
-func NewRouter() http.Handler {
-	postgresDSN := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASS"),
-		os.Getenv("DB_NAME"))
 
-	db, err := sql.Open("postgres", postgresDSN)
+func NewRouter() http.Handler {
+
+	db, err := sql.Open("sqlite3", "steam-trade-server.db")
 	if err != nil {
 		panic(err)
 	}
