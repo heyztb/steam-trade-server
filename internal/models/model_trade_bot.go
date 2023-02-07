0003f5bf97138041e00d64b6c828c1a876166829
Zach Blake <zblake@sightlinepayments.com>
2023-02-07 13:57:50 -0800

    trade func and more event handling

--- internal/models/model_trade_bot.go
+++ internal/models/model_trade_bot.go
@@ -5,9 +5,12 @@ import (
 	"time"
 
 	"github.com/Philipp15b/go-steam/v3"
+	"github.com/Philipp15b/go-steam/v3/steamid"
 	"github.com/Philipp15b/go-steam/v3/totp"
+	"github.com/Philipp15b/go-steam/v3/trade/tradeapi"
 	"github.com/Philipp15b/go-steam/v3/tradeoffer"
 	"github.com/heyztb/steam-trade-server/internal/database"
+	"github.com/heyztb/steam-trade-server/pkg/tradeurl"
 	"github.com/pkg/errors"
 )
 
@@ -20,10 +23,25 @@ type TradeBot struct {
 func (bot *TradeBot) EventHandler() {
 	for event := range bot.Client.Events() {
 		switch e := event.(type) {
+		case *steam.FatalErrorEvent:
+			bot.Logger.Printf("Fatal error")
+			return
 		case *steam.LoggedOnEvent:
 			bot.Logger.Printf("Log on succeeded: %s", e.ClientSteamId.String())
 		case *steam.LogOnFailedEvent:
 			bot.Logger.Printf("Log on failed: %+v\n", e.Result)
+			return
+		case *steam.WebSessionIdEvent:
+			bot.Client.Web.LogOn()
+		case *steam.TradeResultEvent:
+			switch e.Response {
+			case 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 22, 32, 33:
+				bot.Logger.Printf("Error with trade offer: %d", e.Response)
+				return
+			case 0, 50:
+				bot.Logger.Printf("Good trade: %d", e.Response)
+				return
+			}
 		}
 	}
 }
@@ -48,8 +66,65 @@ func (bot *TradeBot) Login() error {
 	return nil
 }
 
-func (bot *TradeBot) Trade(tradeUrl string, ourItems []Asset, theirItems []Asset) {
-	_ = tradeoffer.NewClient("", bot.Client.Web.SessionId, bot.Client.Web.SteamLogin, bot.Client.Web.SteamLoginSecure)
+func (bot *TradeBot) Trade(tradeUrl string, ourItems []Asset, theirItems []Asset) (*uint64, error) {
+	tradeClient := tradeoffer.NewClient(tradeoffer.APIKey(bot.Credentials.ApiKey), bot.Client.Web.SessionId, bot.Client.Web.SteamLogin, bot.Client.Web.SteamLoginSecure)
 
-	// TODO: Implement
+	tradeInfo, err := tradeurl.Parse(tradeUrl)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to parse trade url")
+	}
+
+	partnerId := tradeInfo[0]
+	accessToken := tradeInfo[1]
+
+	theirId, err := steamid.NewId(partnerId)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to parse steam ID")
+	}
+
+	us := []tradeoffer.TradeItem{}
+	them := []tradeoffer.TradeItem{}
+
+	for _, item := range ourItems {
+		us = append(us, tradeoffer.TradeItem{
+			AppId:      uint32(item.AppID),
+			ContextId:  2,
+			Amount:     1,
+			AssetId:    uint64(item.AssetID),
+			CurrencyId: 0,
+		})
+	}
+
+	for _, item := range theirItems {
+		them = append(them, tradeoffer.TradeItem{
+			AppId:      uint32(item.AppID),
+			ContextId:  2,
+			Amount:     1,
+			AssetId:    uint64(item.AssetID),
+			CurrencyId: 0,
+		})
+	}
+
+	offerId, err := tradeClient.Create(theirId, &accessToken, us, them, nil, "")
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to create trade offer")
+	}
+
+	return &offerId, nil
+}
+
+func (bot *TradeBot) Confirm(other steamid.SteamId) error {
+	tradeApiClient := tradeapi.New(bot.Client.Web.SessionId, bot.Client.Web.SteamLogin, bot.Client.Web.SteamLoginSecure, other)
+
+	_, err := tradeApiClient.SetReady(true)
+	if err != nil {
+		return errors.Wrapf(err, "unable to set ready on trade w/ %s", other.String())
+	}
+
+	_, err = tradeApiClient.Confirm()
+	if err != nil {
+		return errors.Wrapf(err, "unable to confirm trade w/ %s", other.String())
+	}
+
+	return nil
 }
