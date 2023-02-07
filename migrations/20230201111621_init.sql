df52b133c77a16d9bafa2b9536affe579db884b7
Zach Blake <zblake@sightlinepayments.com>
2023-02-07 13:57:55 -0800

    update schema

--- migrations/20230201111621_init.sql
+++ migrations/20230201111621_init.sql
@@ -5,7 +5,8 @@ create table bots (
   username text not null,
   passwd text not null,
   shared_secret text not null,
-  identity_secret text not null
+  identity_secret text not null,
+  api_key text not null
 );
 
 create table items (
@@ -21,6 +22,7 @@ create table items (
 create unique index idx_bots_username on bots(username);
 create unique index idx_bots_shared_secret on bots(shared_secret);
 create unique index idx_bots_identity_secret on bots(identity_secret);
+create unique index idx_bots_api_key on bots(api_key);
 
 create unique index idx_unique_item on items(app_id, asset_id);
 create index idx_items_app_id on items(app_id);
@@ -32,6 +34,7 @@ create index idx_items_asset_id on items(asset_id);
 drop index idx_bots_username;
 drop index idx_bots_shared_secret;
 drop index idx_bots_identity_secret;
+drop index idx_bots_api_key;
 drop index idx_unique_item;
 drop index idx_items_app_id;
 drop index idx_items_asset_id;
